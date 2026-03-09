74b479eb2b38aa4f73938ec698cc2e5ddcf33d11
koweteagle <koweteagle@gmail.com>
2026-03-09 15:51:34 +0100

    dasdsada#   (use "git push" to publish your local commits)

--- game.js
+++ game.js
@@ -208,8 +208,8 @@ const BOSS_CONFIG = {
     boss2: { width: 300, height: 400, scale: 1.5, speed: 2.5, downScale: 0.7,    downOffset: 0, offset: 0,  mirrorFlip: true,  throwCount: 2, throwTimeToTarget: 60,  throwHitChance: 0.7, throwDamage: 6 },
     boss3: { width: 250, height: 350, scale: 1.4, speed: 2.5, downScale: 0.7,    downOffset: 0, offset: 0,  mirrorFlip: true,  throwCount: 1, throwTimeToTarget: 50,  throwHitChance: 0.8, throwDamage: 6 },
     boss4: { width: 260, height: 420, scale: 1.4, speed: 2.5, downScale: 0.75,   downOffset: 0, offset: 0,  mirrorFlip: true, throwCount: 2, throwTimeToTarget: 55,  throwHitChance: 0.7, throwDamage: 7 }, // Peperbus
-    boss5: { width: 250, height: 350, scale: 1,   speed: 2.5, downScale: 1,      downOffset: 0, offset: 0,  mirrorFlip: true, throwCount: 1, throwTimeToTarget: 40,  throwHitChance: 0.8, throwDamage: 8 }, // Dominguez
-    boss6: { width: 220, height: 380, scale: 1.2, speed: 2.5, downScale: 0.8,    downOffset: 0, offset: 0,  mirrorFlip: false, throwCount: 1, throwTimeToTarget: 50,  throwHitChance: 0.65, throwDamage: 5 }  // ME – gooit stok
+    boss5: { width: 250, height: 350, scale: 1,   speed: 2.5, downScale: 1,      downOffset: 0, offset: 0,  mirrorFlip: false, throwCount: 1, throwTimeToTarget: 40,  throwHitChance: 0.8, throwDamage: 8 }, // Dominguez
+    boss6: { width: 220, height: 380, scale: 1.2, speed: 2.5, downScale: 0.8,    downOffset: 0, offset: 0,  mirrorFlip: true, throwCount: 1, throwTimeToTarget: 50,  throwHitChance: 0.65, throwDamage: 5 }  // ME – gooit stok
 };
 function getBossConfig(type) {
     return BOSS_CONFIG[type] || { width: 250, height: 350, scale: 1, speed: 2.5, downScale: 1, downOffset: 0, offset: 0, mirrorFlip: false, throwCount: 1, throwTimeToTarget: 50, throwHitChance: 0.7, throwDamage: 6 };
