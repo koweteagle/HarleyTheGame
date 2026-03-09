b03812c91e278910eee2bcbad069428223d0a5f7
koweteagle <koweteagle@gmail.com>
2026-03-09 11:48:37 +0100

    boer2 2

--- game.js
+++ game.js
@@ -197,7 +197,7 @@ const HOOLIGAN_VX_WORLD_OFFSET = 6;   // extra t.o.v. BASE_WORLD_SPEED voor vx
 const BOSS_CONFIG = {
     boss0: { width: 260, height: 350, scale: 1.7, speed: 2.5, downScale: 0.8,    downOffset: 0, offset: 25, mirrorFlip: true, throwCount: 1, throwTimeToTarget: 80, throwHitChance: 0.5, throwDamage: 2 },
     boss1: { width: 350, height: 450, scale: 1,   speed: 2.5, downScale: 1,    downOffset: 0, offset: 0, mirrorFlip: true, throwCount: 6, throwTimeToTarget: 100, throwHitChance: 0.3, throwDamage: 1 },
-    boss2: { width: 300, height: 400, scale: 1,   speed: 2.5, downScale: 1,    downOffset: 0, offset: 0, mirrorFlip: false, throwCount: 1, throwTimeToTarget: 60, throwHitChance: 0.7, throwDamage: 6 },
+    boss2: { width: 300, height: 400, scale: 1.5, speed: 2.5, downScale: 1.4,    downOffset: 0, offset: 0, mirrorFlip: true, throwCount: 2, throwTimeToTarget: 60, throwHitChance: 0.7, throwDamage: 6 },
     boss3: { width: 250, height: 350, scale: 1,   speed: 2.5, downScale: 1,    downOffset: 0, offset: 0, mirrorFlip: false, throwCount: 1, throwTimeToTarget: 50, throwHitChance: 0.7, throwDamage: 6 },
     boss4: { width: 250, height: 350, scale: 1,   speed: 2.5, downScale: 1,    downOffset: 0, offset: 0, mirrorFlip: false, throwCount: 1, throwTimeToTarget: 40, throwHitChance: 0.8, throwDamage: 8 }
 };
