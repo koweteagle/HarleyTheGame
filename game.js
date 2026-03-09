2772b269383e5a2b936cc22060b156050b6b2c8f
koweteagle <koweteagle@gmail.com>
2026-03-09 15:28:10 +0100

    peperbus3

--- game.js
+++ game.js
@@ -206,7 +206,7 @@ const BOSS_CONFIG = {
     boss1: { width: 350, height: 450, scale: 1,   speed: 2.5, downScale: 1,      downOffset: 0, offset: 0,  mirrorFlip: true,  throwCount: 6, throwTimeToTarget: 100, throwHitChance: 0.3, throwDamage: 1 },
     boss2: { width: 300, height: 400, scale: 1.5, speed: 2.5, downScale: 0.7,    downOffset: 0, offset: 0,  mirrorFlip: true,  throwCount: 2, throwTimeToTarget: 60,  throwHitChance: 0.7, throwDamage: 6 },
     boss3: { width: 250, height: 350, scale: 1.4, speed: 2.5, downScale: 0.7,    downOffset: 0, offset: 0,  mirrorFlip: true,  throwCount: 1, throwTimeToTarget: 50,  throwHitChance: 0.8, throwDamage: 6 },
-    boss4: { width: 260, height: 420, scale: 1.4, speed: 2.5, downScale: 0.75,   downOffset: 0, offset: 0,  mirrorFlip: false, throwCount: 2, throwTimeToTarget: 55,  throwHitChance: 0.7, throwDamage: 7 }, // Peperbus
+    boss4: { width: 260, height: 420, scale: 1.4, speed: 2.5, downScale: 0.75,   downOffset: 0, offset: 0,  mirrorFlip: true, throwCount: 2, throwTimeToTarget: 55,  throwHitChance: 0.7, throwDamage: 7 }, // Peperbus
     boss5: { width: 250, height: 350, scale: 1,   speed: 2.5, downScale: 1,      downOffset: 0, offset: 0,  mirrorFlip: false, throwCount: 1, throwTimeToTarget: 40,  throwHitChance: 0.8, throwDamage: 8 }  // Dominguez (oude boss4)
 };
 function getBossConfig(type) {
